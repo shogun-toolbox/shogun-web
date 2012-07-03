003838119535fd8994c7bda2821c34417c47692f
Bernard Hernandez <bernard.hernandez.perez@gmail.com>
2012-07-04 01:39:53 +0200

    + /shogun, /templates, db

---
+++ shogun/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'shogun.views.home', name='home'),
+    # url(r'^shogun/', include('shogun.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Other urls:
+    # Subpages.
+    url(r'^home', 'pages.views.home'),
+    url(r'^page/(?P<page>[\w|\d]+)/(?P<subpage>[\w|\d]+)', 'pages.views.pageHandler'),
+
+    # Main page.
+    (r'^', 'pages.views.home'),
+)
