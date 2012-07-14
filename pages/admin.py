9b28a63dd499b222a04e858847f1c1154ec6e0f2
Bernard Hernandez <bernard.hernandez.perez@gmail.com>
2012-07-14 20:04:33 +0200

    + small editor HTML tiny_mce added to edit textfields

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,5 @@
+
+# Import objetts.
 from pages.models import Article
 from pages.models import Subpage
 from pages.models import Page
@@ -5,7 +7,16 @@ from pages.models import New
 
 from django.contrib import admin
 
-admin.site.register(Article)
+# HTML Editor
+class ArticleOptions(admin.ModelAdmin):
+	class Media:
+		js = ('../static/js/tiny_mce/tiny_mce.js',
+			  '../static/js/editors/textfield.js')
+
+
+# Objects editable by admin.
+admin.site.register(Article, ArticleOptions)
 admin.site.register(Subpage)
 admin.site.register(Page)
 admin.site.register(New)
+
