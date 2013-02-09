d3b05f89c38bfec732e4b23438c0abfdbcebd817
Soeren Sonnenburg <sonne@debian.org>
2013-02-09 22:13:16 +0100

    add iframe to allowed fields in tinymce

--- static/js/tiny_mce/tine_mce_config.js
+++ static/js/tiny_mce/tine_mce_config.js
@@ -3,7 +3,8 @@
 tinyMCE.init({
 	theme : "advanced",
 	mode : "textareas",
-	theme_advanced_buttons3_add: 'code',
+	theme_advanced_buttons3_add: "code",
+	extended_valid_elements : "iframe[src|width|height|name|align]",
 	verify_html : false
 });
 </script>
