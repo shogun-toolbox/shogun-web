007ae0677726c2c3e1a795c5248b49897e424c8f
Soeren Sonnenburg <sonne@debian.org>
2013-10-07 21:57:26 +0200

    fix js / css errors

--- static/js/overlay.js
+++ static/js/overlay.js
@@ -1,16 +1,16 @@
-$(document).on("click", ".overlay", function(){
-        event.preventDefault();
-	$('body').css('overflow', 'hidden');
-    $(".overlay-content").load($(this).attr("href"),
-      function(){$(".overlay-bg").fadeIn(500); 
+$(document).ready(function() {
+	$(".overlay").click(function(event) {
+		event.preventDefault();
+		$('body').css('overflow', 'hidden');
+		$(".overlay-content").load($(this).attr("href"),
+			function() {
+				$(".overlay-bg").fadeIn(500); 
+			});
+	});
 
-		  $.getScript("http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML");
-	  
-	  })
-});
-
-$(document).on("click", ".overlay-close", function(event) {
-    event.preventDefault();
-    $(".overlay-bg").fadeOut(500, function() { $(".overlay-content").empty();});
-	$('body').css('overflow', 'visible');
+	$(".overlay-close").click(function(event) {
+		event.preventDefault();
+		$(".overlay-bg").fadeOut(500, function() { $(".overlay-content").empty();});
+		$('body').css('overflow', 'visible');
+	});
 });
