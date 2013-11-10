01b95f502cc28929d10d2a56b3da859fe918266f
Soeren Sonnenburg <sonne@debian.org>
2013-11-10 20:09:16 +0100

    fix another typo

--- util/export.py
+++ util/export.py
@@ -5,7 +5,7 @@ from util import notebook
 from django.http import HttpResponse,Http404
 
 def list_notebooks(request):
-	response_data=[(nb[3],nb[2]) for nb in notebook.get_notebook_list()]
+	response_data=[(nb[3],nb[2]) for nb in notebook.get_notebook_list("*.ipynb")]
 	response=HttpResponse(json.dumps(response_data), content_type="application/json")
 	response['Access-Control-Allow-Origin']='*';
 	return response
